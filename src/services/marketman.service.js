fe08c615cf8c933e2f7fe052d0de9e5b720f813e
ignacio salinas <50337855+dublind@users.noreply.github.com>
2026-03-30 21:12:10 -0300

    Fix lines 84-86 to use the correct format in marketman.service.js

--- src/services/marketman.service.js
+++ src/services/marketman.service.js
@@ -1,4 +1,3 @@
-// ... other code
-const fromDateUTC = startDate;
-const toDateUTC = startDate;
-// ... other code
+const fromDateUTC = `${startDate.replace(/-/g, '/')} 00:00:00`;
+const toDateUTC = `${startDate.replace(/-/g, '/')} 23:59:59`;
+const containerID = startDate.replace(/-/g, '');
